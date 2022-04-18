d1c01c329f9c20985b0109501e803d4ed068d2f7
Sandeep Kumar Jakkaraju <sandeepkumar.jakkaraju@gmail.com>
2022-04-18 17:04:22 +0530

    first commit

---
+++ src/main/java/com/incedo/kirke/rag/ragapp/RagappApplication.java
@@ -0,0 +1,13 @@
+package com.incedo.kirke.rag.ragapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/incedo/kirke/rag/ragapp/RagappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incedo.kirke.rag.ragapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b68724580a835dee70acecb9026546c27243355b
Sandeep Kumar Jakkaraju <sandeepkumar.jakkaraju@gmail.com>
2022-04-22 01:04:28 +0530

    working login code

--- src/main/java/com/incedo/kirke/rag/ragapp/RagappApplication.java
+++ src/main/java/com/incedo/kirke/rag/ragapp/RagappApplication.java
@@ -2,12 +2,12 @@ package com.incedo.kirke.rag.ragapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 public class RagappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RagappApplication.class, args);
 	}
-
 }
